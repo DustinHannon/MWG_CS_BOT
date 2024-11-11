0f774a6b2e79e623f77ea82f7e6bc325d34befda
Dustin Hannon <dustin.hannon@morganwhite.com>
2024-11-11 09:50:06 -0600

    update to prompt info

--- ChatBot/server/utils.js
+++ ChatBot/server/utils.js
@@ -12,6 +12,7 @@ export function enrichUserPromptWithContext(prompt) {
     Our customer service hours are Monday through Thursday, 8:00am until 5:00pm and Friday, 8:30am until 2:30pm. We are closed on Saturday and Sunday.
     If the customer is mad, upset, or the answer can not be found, ask the customer to please call our customer service number.
     Do not use your knowledge to answer questions that might have legal side effects. If anything like that comes up, just ask the customer to call our customer service number.
+    Do not answer any questions outside of the medical, health, dental, and vision insurance field. If the question is outside of this field, ask the customer to call our customer service number.
     
     Here are some examples of how the bot might respond to a customer's question:
     Customer: Hello, I am very upset. My insurance is not taken at my primary doctor!
